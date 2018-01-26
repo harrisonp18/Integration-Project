bdd1f477f30d3cc0ad00f4dc7e40a5a2116fb9ba
hpaxton8508 <hpaxton8508@L438R3S6.STUDENT.ad.fgcu.edu>
2018-01-25 19:01:04 -0500

    Changes made in class

--- HarrisonIntegration/src/Main.java
+++ HarrisonIntegration/src/Main.java
@@ -2,31 +2,26 @@ import java.util.Scanner;
 
 //Harrison Paxton
 //My project that integrates all the skills I've learned.
+
+//A variable is a location in memory or a placeholder for something such as a primitive data type (int, double, etc.)
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Hello World!");
+		System.out.println("Hello!");
 		//sysout ctrl+space
 		
 		System.out.println("What is your name?");
 		String userName = scan.nextLine();
 		
-		System.out.println("Enter x1");//prompt for input
-		int x1 = scan.nextInt();//get in input
+		System.out.println("How old are you?");//prompt for input
+		int age = scan.nextInt();//get in input
 		
-		System.out.println("Enter rate");//prompt
-		double rate = scan.nextDouble();
+		System.out.println("Your name is " + userName + " and you are " + age + " years old.");//prompt
 		
-		Dog spot = new Dog();
-		spot.size = 30;
-		Dog spuds = new Dog();
-		spuds.size = 40;
+		
+	
 		
 	}
 
-}
-
-class Dog {
-	double size;
 }
