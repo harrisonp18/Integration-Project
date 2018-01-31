531843b1a6f31bde03548a5283f7fad2a4844195
hpaxton8508 <hpaxton8508@L438R3S6.STUDENT.ad.fgcu.edu>
2018-01-30 19:16:08 -0500

    Changed program to solar system weight scale added all planets and conversion equations

--- HarrisonIntegration/src/Main.java
+++ HarrisonIntegration/src/Main.java
@@ -1,26 +1,49 @@
 import java.util.Scanner;
 
 //Harrison Paxton
-//My project that integrates all the skills I've learned.
+//My project that integrates all the skills I've learned. This project will tell you the weight of something on any planet in our solar system.
 
 //A variable is a location in memory or a placeholder for something such as a primitive data type (int, double, etc.)
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Hello!");
+		System.out.println("Solar System Weight Scale");
 		//sysout ctrl+space
+	
+		System.out.println("Enter a weight (lbs):");
+		int userWeight = scan.nextInt();
 		
-		System.out.println("What is your name?");
-		String userName = scan.nextLine();
+		System.out.println("Pick a planet:");//prompt for input
+		scan.nextLine();
+		String planet = scan.nextLine();//get in input
 		
-		System.out.println("How old are you?");//prompt for input
-		int age = scan.nextInt();//get in input
+		double planetWeight = 0;
 		
-		System.out.println("");
-		
-		System.out.println("Your name is " + userName + " and you are " + age + " years old.");//prompt
-		
+		switch (planet) {
+			case "Mercury":
+				planetWeight = userWeight*0.38;
+				break;
+			case "Venus":
+				planetWeight = userWeight*0.91;
+				break;
+			case "Mars":
+				planetWeight = userWeight*0.38;
+				break;
+			case "Jupiter":
+				planetWeight = userWeight*2.34;
+				break;
+			case "Saturn":
+				planetWeight = userWeight*1.06;
+				break;
+			case "Neptune":
+				planetWeight = userWeight*0.92;
+				break;
+			case "Uranus":
+				planetWeight = userWeight*1.19;
+				break;
+		}
+		System.out.println(userWeight + " lbs on Earth is equivalent to " + planetWeight + " lbs on " + planet + ".");
 		
 	}
 
