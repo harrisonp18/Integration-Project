c1846b58bffa3da12bb137b8ea97194c0d82ab81
Harrison Paxton <harrisonpaxton18@gmail.com>
2018-02-20 18:18:13 -0500

    edited car class

--- HarrisonIntegration/src/Car.java
+++ HarrisonIntegration/src/Car.java
@@ -4,6 +4,16 @@ public class Car {
   private int speed;
   private String make;
   
+  public Car() {
+    speed = 0;
+    make = "unknown";
+  }
+  
+  public Car(int speed, String make) {
+    this.speed=speed;
+    this.make=make;
+  }
+  
   public void setSpeed(int speed) {
     this.speed=speed;
   }
