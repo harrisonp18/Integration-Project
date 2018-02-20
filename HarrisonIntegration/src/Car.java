3d4e69317e976209a6b578dd7d0830ded4b37e33
Harrison Paxton <harrisonpaxton18@gmail.com>
2018-02-20 18:12:50 -0500

    created car class

---
+++ HarrisonIntegration/src/Car.java
@@ -0,0 +1,24 @@
+
+public class Car {
+  
+  private int speed;
+  private String make;
+  
+  public void setSpeed(int speed) {
+    this.speed=speed;
+  }
+  
+  public int getSpeed() {
+    return speed;
+  }
+  
+  public void setMake(String make) {
+    this.make=make;
+  }
+
+  public String getMake() {
+    return make;
+  }
+  
+}
+
