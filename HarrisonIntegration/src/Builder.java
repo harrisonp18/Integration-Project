7b02f87f703b231732673183099b68ff43885067
Harrison Paxton <harrisonpaxton18@gmail.com>
2018-03-13 19:15:42 -0400

    Builder file done to satisfy StringBuilder requirement

---
+++ HarrisonIntegration/src/Builder.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+//Harrison Paxton
+//This program demonstrates three methods of the String Builder class
+
+public class Builder {
+  
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Enter a word:");
+    String userString = scan.next();
+    StringBuilder userS = new StringBuilder(userString);
+
+    userS.reverse();
+    System.out.println("Your Input Backwards: " +userS);
+    
+    System.out.println("Enter a second word: ");
+    String userString2 = scan.next();
+    
+    userS.append(userString2);
+    System.out.println("Your second word has been appended to your"
+        + " first: " + userS);
+    
+    int space = userString.length();
+    userS.insert(space , " ");
+    System.out.println("A space was added in the middle: " + userS);
+  }
+}
