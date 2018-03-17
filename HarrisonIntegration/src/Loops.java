e5dba9bb291d9ca559f6bc867d266d82f3feb48f
Harrison Paxton <harrisonpaxton18@gmail.com>
2018-03-17 13:40:10 -0400

    started loops

---
+++ HarrisonIntegration/src/Loops.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class WhileDemo {
+  public static void main(String[] args) {
+    int count = 1;
+    while (count < 11) {
+      System.out.println("Count is: " + count);
+      count++;
+    }
+  }
+}
